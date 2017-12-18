c8f654fa24b8b27f8abfd232d5a19ec7d7719700
Tim <lwpatent@yahoo.com>
2017-12-18 11:35:43 -0500

    LL

---
+++ web-advanced/server.rb
@@ -0,0 +1,7 @@
+# @Author: timaanonsen
+# @Date:   12-18-2017 11:12::67
+# @Filename: server.rb
+# @Last modified by:   timaanonsen
+# @Last modified time: 12-18-2017 11:12::94
+
+requirer 'sinatra'
